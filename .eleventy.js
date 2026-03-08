a22a2ca084a75fdaa3ec454de89c33dfefd6e7d8
jonnyy04 <151456268+jonnyy04@users.noreply.github.com>
2026-03-08 14:24:10 +0200

    Add pathPrefix for GitHub Pages image paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPassthroughCopy("src/img");
 	eleventyConfig.addPassthroughCopy("src/styles.css");
 	eleventyConfig.addPassthroughCopy("src/admin");
 
 	return {
+		pathPrefix: process.env.PATH_PREFIX || "/",
 		dir: {
 			input: "src",
 			output: "dist",
