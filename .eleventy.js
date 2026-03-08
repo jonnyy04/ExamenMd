ca7817a7e0220bf9aff4956e1c848a46eabc563d
jonnyy04 <151456268+jonnyy04@users.noreply.github.com>
2026-03-08 21:07:26 +0200

    Make site content dynamic

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,26 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
+	eleventyConfig.addFilter("localeString", function (value) {
+		return Number(value).toLocaleString("ro-RO");
+	});
+
 	eleventyConfig.addPassthroughCopy("src/img");
 	eleventyConfig.addPassthroughCopy("src/styles.css");
 	eleventyConfig.addPassthroughCopy("src/admin");
+
+	eleventyConfig.addCollection("courses", function (collectionApi) {
+		return collectionApi.getFilteredByGlob("src/courses/*.md").sort((a, b) => {
+			return (a.data.order || 0) - (b.data.order || 0);
+		});
+	});
+
+	eleventyConfig.addCollection("teachers", function (collectionApi) {
+		return collectionApi.getFilteredByGlob("src/teachers/*.md").sort((a, b) => {
+			return (a.data.order || 0) - (b.data.order || 0);
+		});
+	});
 
 	return {
 		pathPrefix: process.env.PATH_PREFIX || "/",
